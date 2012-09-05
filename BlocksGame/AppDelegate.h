15364c1daf8dd58a5d03756315fcd557950cdfe1
Maneesh Goel <maneeshgoel@gmail.com>
2012-09-05 15:34:36 -0700

    initial commit

---
+++ BlocksGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BlocksGame
+//
+//  Created by Maneesh Goel on 9/5/12.
+//  Copyright (c) 2012 Maneesh Goel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
