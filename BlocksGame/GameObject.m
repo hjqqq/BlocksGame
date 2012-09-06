e3e3411434c704bf4696462fada3b5fa35baac98
Maneesh Goel <maneeshgoel@gmail.com>
2012-09-05 18:46:23 -0700

    working recursive tap delete

--- BlocksGame/GameObject.m
+++ BlocksGame/GameObject.m
@@ -8,6 +8,9 @@
 
 #import "GameObject.h"
 #import "Shape.h"
+@interface GameObject()
+@property NSMutableArray *shapesToRemove;
+@end
 
 @implementation GameObject
 @synthesize grid = _grid;
@@ -18,6 +21,7 @@
     if (self) {
         _colors = [NSArray arrayWithObjects:[UIColor redColor], [UIColor greenColor], [UIColor whiteColor], [UIColor yellowColor], [UIColor orangeColor] , nil];
         [self setupGrid];
+        self.shapesToRemove = [NSMutableArray new];
     }
     return self;
 }
@@ -31,10 +35,11 @@
             [array addObject:shape];
         }
     }
-    NSLog(@"grid is:\n %@", self.grid);
+    //NSLog(@"grid is:\n %@", self.grid);
 }
 
 -(void)handleTouchAtX:(int)xPosition andY:(int)yPosition {
+    self.shapesToRemove = [NSMutableArray new];
     NSLog(@"checking position: %d, %d", xPosition, yPosition);
     NSLog(@"count at xPosition is %d", [[self.grid objectAtIndex:xPosition] count]);
     if (xPosition > 7 || xPosition <0 ) {
@@ -44,10 +49,59 @@
         return;
     }
     
+    [self removeShapeAtX:xPosition andY:yPosition andColor:[[[self.grid objectAtIndex:xPosition] objectAtIndex:yPosition] color]];
+        //NSLog(@"grid is:\n %@", self.grid);
+}
+
+-(void)removeShapeAtX:(int)xPosition andY:(int)yPosition andColor:(UIColor *)color {
+    NSLog(@"checking at %d, %d", xPosition, yPosition);
     Shape* shapeToRemove = [[self.grid objectAtIndex:xPosition] objectAtIndex:yPosition];
+
+    UIColor *initialColor = color;
+    NSLog(@"Shapes Color: %@", initialColor);
     if (shapeToRemove) {
-        [[self.grid objectAtIndex:xPosition] removeObject:shapeToRemove];
+        NSLog(@"adding %d, %d to removal array", xPosition, yPosition);
+        [self.shapesToRemove addObject:shapeToRemove];
+        //figure out which adjacent shapes to remove
+        //look 1 right
+        if (xPosition < 7 && yPosition <[[self.grid objectAtIndex:xPosition+1] count]) {
+            NSLog(@"checking right at %d, %d", xPosition+1, yPosition);
+            Shape *right = [[self.grid objectAtIndex:xPosition+1] objectAtIndex:yPosition];
+            if ([[right color] isEqual:initialColor] && ![self.shapesToRemove containsObject:right]) {
+                [self removeShapeAtX:xPosition+1 andY:yPosition andColor:initialColor];
+            }
+        }
+        //look 1 left
+        if (xPosition > 0 &&  yPosition < [[self.grid objectAtIndex:xPosition-1] count]) {
+            NSLog(@"checking left at %d, %d", xPosition-1, yPosition);
+            Shape *left = [[self.grid objectAtIndex:xPosition-1] objectAtIndex:yPosition];
+            if ([[left color] isEqual:initialColor] && ![self.shapesToRemove containsObject:left]) {
+                [self removeShapeAtX:xPosition-1 andY:yPosition andColor:initialColor];
+            }
+        }
+        //look 1 up
+        if (yPosition < [[self.grid objectAtIndex:xPosition] count]-1) {
+            NSLog(@"checking up at %d, %d", xPosition, yPosition+1);
+            Shape *up = [[self.grid objectAtIndex:xPosition] objectAtIndex:yPosition+1];
+            if ([[up color] isEqual:initialColor] && ![self.shapesToRemove containsObject:up]) {
+                [self removeShapeAtX:xPosition andY:yPosition+1 andColor:initialColor];
+            }
+        }
+        //look 1 down
+        if (yPosition > 0 && yPosition < [[self.grid objectAtIndex:xPosition] count]) {
+            NSLog(@"checking down at %d, %d", xPosition, yPosition-1);
+            Shape *down = [[self.grid objectAtIndex:xPosition] objectAtIndex:yPosition-1];
+            if ([[down color] isEqual:initialColor] && ![self.shapesToRemove containsObject:down]) {
+                [self removeShapeAtX:xPosition andY:yPosition-1 andColor:initialColor];
+            }
+        }
+        //remove all objects in shapesToRemove
+        for (Shape *shape in self.shapesToRemove) {
+            [[self.grid objectAtIndex:xPosition] removeObject:shape];
+        }
+    
     }
-    NSLog(@"grid is:\n %@", self.grid);
+
+    
 }
 @end
